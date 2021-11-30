b59db63864d87ecf60af4bfbab0db13538c1c44d
Viet <nguyen.viet@nokia.com>
2021-11-30 22:04:50 +0100

    upgrade

--- simulation.cpp
+++ simulation.cpp
@@ -7,12 +7,20 @@
 
 #define SIZEX 10
 #define SIZEY 10
+#define FPS 1
 
+ 
 
+struct Node{
+    int locX;
+    int locY;
+};
 
-int get_location(std::vector<int> loc, std::vector<std::vector<int>> setLoc){
+ 
+
+int get_location(Node currLoc, std::vector<Node> setLoc){
     for( int i = 0; i<setLoc.size();i++){
-        if( loc[0] == setLoc[i][0] && loc[1] == setLoc[i][1])
+        if( currLoc.locX == setLoc[i].locX && currLoc.locY == setLoc[i].locY)
             return i;
     }
     return -1;
@@ -20,13 +28,12 @@ int get_location(std::vector<int> loc, std::vector<std::vector<int>> setLoc){
 }
 
 
-void setup(std::vector<std::vector<int>> &location){
-
+void setup(std::vector<Node> location){
     int num_robot = location.size();
     std::vector<int> tmp;
     for ( int i=0;i<SIZEX;i++){
         for ( int j=0;j<SIZEY;j++){
-            tmp = {i,j};
+            Node tmp{i,j};
             if ( get_location(tmp, location) != -1)
                 std::cout<<"| "<<get_location(tmp, location)<<" |";
             else
@@ -38,46 +45,46 @@ void setup(std::vector<std::vector<int>> &location){
         }
         std::cout<<std::endl;
     }
-    sleep(1);
-    }
+    sleep(FPS);
+}
 
-inline int calNorm1(std::vector<int> a,std::vector<int> b){   return (abs(a[0] - b[0]) + abs(a[1]-b[1])); }
+inline int calNorm1(Node a,Node b){   return (abs(a.locX - b.locX + abs(a.locY-b.locY))); }
 
-void releaseRegister(std::vector<std::vector<int>> setLoc,std::vector<std::vector<int>> &mapRegister){
+void releaseRegister(std::vector<Node> setLocRobot,std::vector<std::vector<int>> &mapRegister){
     for ( int row =0; row<SIZEX ;++row){
         for ( int col = 0; col<SIZEY ;++col ){
             if ( mapRegister[row][col] != -1){
-                if (calNorm1(setLoc[mapRegister[row][col]],{row,col}))
+                if (calNorm1(setLocRobot[mapRegister[row][col]],Node{row,col}))
                     mapRegister[row][col] = -1;
             }
         }
     }
 }
 
-void robotRegister(std::vector<std::vector<int>> askLoc,std::vector<std::vector<int>> &mapRegister){
+void robotRegister(std::vector<Node> askLoc,std::vector<std::vector<int>> &mapRegister){
     for ( int rb = 0; rb < askLoc.size(); ++rb){
-        if (mapRegister[askLoc[rb][0]][askLoc[rb][1]] == -1)  mapRegister[askLoc[rb][0]][askLoc[rb][1]] = rb;
+        if (mapRegister[askLoc[rb].locX][askLoc[rb].locY] == -1)  mapRegister[askLoc[rb].locX][askLoc[rb].locY] = rb;
     }
 }
 
-bool askMove(int robotName,std::vector<int> currLoc, std::vector<int> askLoc, std::vector<std::vector<int>> mapRegister){
-    if ( mapRegister[askLoc[0]][askLoc[1]] == robotName) return true;
+inline bool askMove(int robotName, Node askLoc, std::vector<std::vector<int>> mapRegister){
+    if ( mapRegister[askLoc.locX][askLoc.locY] == robotName) return true;
     else return false;
 }
+
 
 
 int main(){
     std::vector<int> elmRegistor(SIZEX,-1);
     std::vector<std::vector<int>> mapRegistor(SIZEY,elmRegistor);
-
-    std::vector<int> s ={1,2};
-    std::vector<std::vector<int>> set = {{0,2},{2,0}};
+ 
+    std::vector<Node> set = {{0,2},{2,0}};
 
     //Robot1 sequence of moving
-    std::vector<std::vector<int>> set1 = {{0,2},{1,2},{2,2},{3,2},{3,3},{3,4},{3,5},{3,6},{3,7},{3,8},{3,9}};
+    std::vector<Node> set1 = {{0,2},{1,2},{2,2},{3,2},{3,3},{3,4},{3,5},{3,6},{3,7},{3,8},{3,9}};
     
     //Robot2 sequence of moving
-    std::vector<std::vector<int>> set2 = {{2,0},{2,1},{2,2},{2,3},{3,3},{4,3},{5,3},{6,3},{7,3},{8,3},{9,3}};
+    std::vector<Node> set2 = {{2,0},{2,1},{2,2},{2,3},{3,3},{4,3},{5,3},{6,3},{7,3},{8,3},{9,3}};
    
     //register at the beginning
     robotRegister(set,mapRegistor);
@@ -102,7 +109,7 @@ int main(){
 
 
         // set of requesting moves
-        std::vector<std::vector<int>> setAsk ;
+        std::vector<Node> setAsk ;
         
         setAsk.push_back(set1[r1Indx]);
         setAsk.push_back(set2[r2Indx]);
@@ -111,8 +118,8 @@ int main(){
         robotRegister(setAsk,mapRegistor);
 
         //Robot try to get the next move
-        if (askMove(0,set1[r1Indx-1],set1[r1Indx],mapRegistor)&& r1Indx<set1.size()-1) r1Indx++;
-        if (askMove(1,set2[r2Indx-1],set2[r2Indx],mapRegistor)&&r2Indx<set2.size()-1) r2Indx++;
+        if (askMove(0, set1[r1Indx],mapRegistor)&& r1Indx<set1.size()-1) r1Indx++;
+        if (askMove(1, set2[r2Indx],mapRegistor)&&r2Indx<set2.size()-1) r2Indx++;
 
         set[0] = set1[r1Indx-1];
         set[1] = set2[r2Indx-1];
